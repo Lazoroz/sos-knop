765b0e0d858523477df747379c0f20d13b075647
SheyaanS <23087013@student.hhs.nl>
2024-01-09 13:45:38 +0100

    controller gefixt

--- src/main/java/com/example/sosknop/Location.java
+++ src/main/java/com/example/sosknop/Location.java
@@ -1,23 +1,27 @@
 package com.example.sosknop;
+
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
 
-public class Location {
+import java.net.URL;
+import java.util.ResourceBundle;
 
-        @FXML
-        private WebView mapView;
+public class Location implements Initializable {
 
-        public void initialize() {
-            WebEngine webEngine = mapView.getEngine();
+    @FXML
+    private WebView mapView;
 
-            try {
-                webEngine.load("https://www.google.com/maps/embed/v1/place?key=AIzaSyAwofV1R-Ce2IlRZWzE6TJ28Q4CDOfRQdI");
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        WebEngine webEngine = mapView.getEngine();
 
+        // Set the initial location (latitude, longitude)
+        double latitude = 37.7749;
+        double longitude = -122.4194;
 
-            } catch (Exception e) {
-                e.printStackTrace();
-                // Handle the exception (log or display an error message)
-            }
-        }
+        // Load Google Maps with the specified location
+        webEngine.load("https://www.google.com/maps?q=" + latitude + "," + longitude);
+    }
 }
