ff09d0c586fff6645654d0b93f65da371026db80
SheyaanS <23087013@student.hhs.nl>
2024-01-11 13:13:17 +0100

    location

--- src/main/java/com/example/sosknop/Location.java
+++ src/main/java/com/example/sosknop/Location.java
@@ -17,14 +17,13 @@ public class Location implements Initializable {
     public void initialize(URL url, ResourceBundle resourceBundle) {
         WebEngine webEngine = mapView.getEngine();
 
-        // Replace these values with the actual latitude and longitude of your location
-        double latitude = 37.7749;
-        double longitude = -122.4194;
+        String apiKey = "AIzaSyAwofV1R-Ce2IlRZWzE6TJ28Q4CDOfRQdI";
 
-        String htmlContent = "<iframe width=\"100%\" height=\"50%\" frameborder=\"0\" style=\"border:0\" "
-                + "src=\"https://www.google.com/maps/embed/v1/view?key=AIzaSyAwofV1R-Ce2IlRZWzE6TJ28Q4CDOfRQdI&center="
-                + latitude + "," + longitude + "&zoom=15\" allowfullscreen></iframe>";
+        String html = "<iframe src=\"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2452.694948138796!2d4.3213990769902075!3d52.06707796966332!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c5b6e175fe3619%3A0x9d1994a880751d7a!2sDe%20Haagse%20Hogeschool!5e0!3m2!1snl!2snl!4v1704895920720!5m2!1snl!2snl\" width=\"380\" height=\"525\" style=\"border:0;\" allowfullscreen=\"\" loading=\"lazy\" referrerpolicy=\"no-referrer-when-downgrade\"></iframe>";
+        html = html.replace("</iframe>", "&key=" + apiKey + " frameborder='0' style='border:0' aria-hidden='false' tabindex='0'></iframe>");
+        webEngine.loadContent(html);
 
-        webEngine.loadContent(htmlContent);
     }
+
 }
+
